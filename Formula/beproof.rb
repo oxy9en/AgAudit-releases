6a36dbf7eed05d4b0140dbf763038a80174ce3ba
Skutin Alexey <alex@aideai.app>
2026-07-31 23:36:23 +0000

    Update beproof formula to 1.0.15

--- Formula/beproof.rb
+++ Formula/beproof.rb
@@ -3,7 +3,7 @@ class Beproof < Formula
   homepage "https://github.com/oxy9en/AgAudit-releases"
   version "1.0.15"
   url "https://github.com/oxy9en/AgAudit-releases/releases/download/1.0.15/beproof-macos-arm64.tar.gz"
-  sha256 "25d9b6746c5bb1ea7d465218924c6a2e227285661314025fa5c36b7766836664"
+  sha256 "0c3f70352d0cfeae2a121335cdfa3930c9208f4abf7e251c1dc00f0b00a175f0"
 
   depends_on macos: :sonoma
 
